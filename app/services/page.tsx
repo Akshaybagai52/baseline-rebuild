67be28bf5f2f281b1d71579fa01759767a375bf7
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-10-25 08:53:17 +0530

    added navlinks

---
+++ app/services/page.tsx
@@ -0,0 +1,16 @@
+
+import Nav from "@/Components/Navbar/Nav";
+
+export default function services() {
+
+    return (
+      <>
+      <Nav/>
+        <div className="">
+          <p>
+            Lorem Ipsum is simply dummy text ...
+          </p>
+        </div>
+      </>
+    );
+  }
